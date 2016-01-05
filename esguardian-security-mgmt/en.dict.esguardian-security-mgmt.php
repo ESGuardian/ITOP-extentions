a388f1f3b586fc5d54f6504fa73c2728e6c55849
ESGuardian <esguardian@outlook.com>
2016-01-05 23:19:40 +0300

    modified:   esguardian-security-mgmt/datamodel.esguardian-security-mgmt.xml 	modified:   esguardian-security-mgmt/en.dict.esguardian-security-mgmt.php 	modified:   esguardian-security-mgmt/model.esguardian-security-mgmt.php 	modified:   esguardian-security-mgmt/module.esguardian-security-mgmt.php 	modified:   esguardian-security-mgmt/ru.dict.esguardian-security-mgmt.php

--- esguardian-security-mgmt/en.dict.esguardian-security-mgmt.php
+++ esguardian-security-mgmt/en.dict.esguardian-security-mgmt.php
@@ -12,6 +12,49 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:Person/Attribute:roles_list' => 'Roles',
 	'Class:Ticket/Attribute:securityroles_list' => 'affected roles',
     'Class:Person/Tab:ConflictingRoles' => 'Conflicts',
+    'Class:Ticket/Attribute:securitycis_list' => 'SCIs',
+	'Class:Ticket/Attribute:securitycis_list+' => 'Security Configuration Items',
+));
+
+//
+// Class: SecurityCI
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:SecurityCI' => 'SCI',
+	'Class:SecurityCI+' => 'Security Configuration Item',
+	'Class:SecurityCI/Attribute:name' => 'Name',
+	'Class:SecurityCI/Attribute:name+' => '',
+	'Class:SecurityCI/Attribute:description' => 'Description',
+	'Class:SecurityCI/Attribute:description+' => '',
+	'Class:SecurityCI/Attribute:org_id' => 'Organization',
+	'Class:SecurityCI/Attribute:org_id+' => '',
+	'Class:SecurityCI/Attribute:org_name' => 'Name',
+	'Class:SecurityCI/Attribute:org_name+' => 'Organization Name',
+	'Class:SecurityCI/Attribute:managers_list' => 'Managers',
+	'Class:SecurityCI/Attribute:managers_list+' => 'Managers of this SCI',
+	'Class:SecurityCI/Attribute:documents_list' => 'Documents',
+	'Class:SecurityCI/Attribute:documents_list+' => '',
+	'Class:SecurityCI/Attribute:tickets_list' => 'Tickets',
+	'Class:SecurityCI/Attribute:tickets_list+' => 'Tickets related to this SCI',
+	'Class:SecurityCI/Attribute:finalclass' => 'SCI Class',
+	'Class:SecurityCI/Attribute:finalclass+' => '',
+	'Class:SecurityCI/Tab:OpenedTickets' => 'Открытые задачи',
+));
+
+//
+// Class: SecurityRoleTemplate
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:SecurityRoleTemplate' => 'Security Role Template',
+	'Class:SecurityRoleTemplate+' => 'Set of Security Roles which can be assigned to person',
+	'Class:SecurityRoleTemplate/Attribute:roles_list' => 'Roles',
+	'Class:SecurityRoleTemplate/Attribute:roles_list+' => '',
+	'Class:SecurityRoleTemplate/Attribute:applyto_list' => 'Assigned to ...',
+	'Class:SecurityRoleTemplate/Attribute:applyto_list+' => '',
+	'Class:SecurityRoleTemplate/Tab:ConflictingRoles' => 'Conflicts',
+    'Class:SecurityRoleTemplate/Tab:ConflictingRoles+' => '',
 ));
 
 
@@ -67,6 +110,105 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:BusinessRole/Attribute:businessprocess_name' => 'Business process Name',
 	'Class:BusinessRole/Attribute:businessprocess_name+' => '',
 ));
+
+//
+// Class: lnkPersonAsManagerOfSecurityCI
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkPersonAsManagerOfSecurityCI' => 'Link:Person as Manager/SCI',
+	'Class:lnkPersonAsManagerOfSecurityCI+' => '',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:securityci_id' => 'SCI',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:securityci_id+' => '',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:securityci_name' => 'SCI',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:securityci_name+' => '',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:person_id' => 'Person',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:person_id+' => '',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:person_name' => 'Surname',
+	'Class:lnkPersonAsManagerOfSecurityCI/Attribute:person_name+' => '',
+));
+
+//
+// Class: lnkSecurityRoleTemplateToSecurityRole
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkSecurityRoleTemplateToSecurityRole' => 'Link:Security Role Template/Security Role',
+	'Class:lnkSecurityRoleTemplateToSecurityRole+' => '',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityroletemplate_id' => 'Security Role Template',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityroletemplate_id+' => '',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityroletemplate_name' => 'Name',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityroletemplate_name+' => 'Security Role Template Name',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityrole_id' => 'Security Role',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityrole_id+' => '',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityrole_name' => 'Name',
+	'Class:lnkSecurityRoleTemplateToSecurityRole/Attribute:securityrole_name+' => 'Security Role Name',
+));
+
+//
+// Class: lnkApplySRTemplateToPerson
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkApplySRTemplateToPerson' => 'Link:Security Role Template/Person',
+	'Class:lnkApplySRTemplateToPerson+' => '',
+	'Class:lnkApplySRTemplateToPerson/Attribute:securityroletemplate_id' => 'Security Role Template',
+	'Class:lnkApplySRTemplateToPerson/Attribute:securityroletemplate_id+' => '',
+	'Class:lnkApplySRTemplateToPerson/Attribute:securityroletemplate_name' => 'Name',
+	'Class:lnkApplySRTemplateToPerson/Attribute:securityroletemplate_name+' => 'Security Role Template Name',
+	'Class:lnkApplySRTemplateToPerson/Attribute:person_id' => 'Person',
+	'Class:lnkApplySRTemplateToPerson/Attribute:person_id+' => '',
+	'Class:lnkApplySRTemplateToPerson/Attribute:person_name' => 'Surname',
+	'Class:lnkApplySRTemplateToPerson/Attribute:person_name+' => '',
+));
+
+//
+// Class: lnkDocumentToSecurityCI
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkDocumentToSecurityCI' => 'Link:Document/SCI',
+	'Class:lnkDocumentToSecurityCI+' => '',
+	'Class:lnkDocumentToSecurityCI/Attribute:securityci_id' => 'SCI',
+	'Class:lnkDocumentToSecurityCI/Attribute:securityci_id+' => '',
+	'Class:lnkDocumentToSecurityCI/Attribute:securityci_name' => 'Name',
+	'Class:lnkDocumentToSecurityCI/Attribute:securityci_name+' => 'SCI name',
+	'Class:lnkDocumentToSecurityCI/Attribute:document_id' => 'Document',
+	'Class:lnkDocumentToSecurityCI/Attribute:document_id+' => '',
+	'Class:lnkDocumentToSecurityCI/Attribute:document_name' => 'Title',
+	'Class:lnkDocumentToSecurityCI/Attribute:document_name+' => 'Document Title',
+));
+
+//
+// Class: lnkSecurityCIToTicket
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkSecurityCIToTicket' => 'Link:SCI/Ticket',
+	'Class:lnkSecurityCIToTicket+' => '',
+	'Class:lnkSecurityCIToTicket/Attribute:ticket_id' => 'Ticket',
+	'Class:lnkSecurityCIToTicket/Attribute:ticket_id+' => '',
+	'Class:lnkSecurityCIToTicket/Attribute:ticket_ref' => 'Number',
+	'Class:lnkSecurityCIToTicket/Attribute:ticket_ref+' => '',
+	'Class:lnkSecurityCIToTicket/Attribute:ticket_title' => 'Title',
+	'Class:lnkSecurityCIToTicket/Attribute:ticket_title+' => '',
+	'Class:lnkSecurityCIToTicket/Attribute:securityci_id' => 'SCI',
+	'Class:lnkSecurityCIToTicket/Attribute:securityci_id+' => '',
+	'Class:lnkSecurityCIToTicket/Attribute:securityci_name' => 'Name',
+	'Class:lnkSecurityCIToTicket/Attribute:securityci_name+' => 'SCI Name',
+	'Class:lnkSecurityCIToTicket/Attribute:impact' => 'Impact',
+	'Class:lnkSecurityCIToTicket/Attribute:impact+' => '',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code' => 'Impact Code',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code+' => '',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code/Value:computed' => 'computed',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code/Value:computed+' => 'computed',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code/Value:manual' => 'manual',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code/Value:manual+' => 'manual',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code/Value:not_impacted' => 'not impacted',
+	'Class:lnkSecurityCIToTicket/Attribute:impact_code/Value:not_impacted+' => 'not impacted',
+));
+
+
 
 //
 // Class: lnkPersonToSecurityRole
@@ -153,9 +295,9 @@ Dict::Add('EN US', 'English', 'English', array(
 //
 
 Dict::Add('EN US', 'English', 'English', array(
-	'Menu:NewRole' => 'Create Role',
-	'Menu:NewRole+' => 'Create Role',
-	'Menu:SearchRoles' => 'Search Roles',
-	'Menu:SearchRoles+' => 'Search Roles',
+	'Menu:NewSecurityCI' => 'Create SCI',
+	'Menu:NewSecurityCI+' => 'Create Security Configuration Item',
+	'Menu:SearchSecurityCIs' => 'Search SCIs',
+	'Menu:SearchSecurityCIs+' => 'Search Security Configuration Items',
 ));
 ?>
