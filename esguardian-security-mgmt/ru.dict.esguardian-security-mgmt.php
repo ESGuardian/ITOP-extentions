ca12d31c292c0399de4b6319fda4eac4eb73dc69
ESGuardian <esguardian@outlook.com>
2016-01-10 01:54:53 +0300

    Почти доделано. Прописана логика для тех. мер ИБ. Прописан русский интерфейс. Осталось прописать логику для орг. мер и английский интерфейс.

--- esguardian-security-mgmt/ru.dict.esguardian-security-mgmt.php
+++ esguardian-security-mgmt/ru.dict.esguardian-security-mgmt.php
@@ -19,6 +19,14 @@ Dict::Add('RU RU', 'Russian', 'Русский', array(
     'Class:Person/Tab:ConflictingRoles+' => 'Список несовместимых ролей связанных с этим человеком',
     'Class:Ticket/Attribute:securitycis_list' => 'КЭБы',
 	'Class:Ticket/Attribute:securitycis_list+' => 'Конфигурационные элементы безопасности связанные с этим тикетом',
+	'Class:Document/Attribute:scis_list' => 'КЭБы',
+	'Class:Document/Attribute:scis_list+' => 'Конфигурационные элементы безопасности связанные с этим тикетом',
+	'Class:Organization/Attribute:scli_list' => 'Правила ИБ',
+	'Class:Organization/Attribute:scli_list+' => 'Список правил информационной безопасности',
+	'Class:Group/Attribute:scli_list' => 'Правила ИБ',
+	'Class:Group/Attribute:scli_list+' => 'Список правил информационной безопасности',
+	'Class:FunctionalCI/Tab:CheckListItems' => 'Проверки',
+	'Class:Organization/Tab:CheckListItems' => 'Проверки',
 ));
 
 //
@@ -249,6 +257,166 @@ Dict::Add('RU RU', 'Russian', 'Русский', array(
 	'Class:lnkSecurityRoleBidirectional/Attribute:right_securityrole_name+' => 'Название роли',
 ));
 
+//
+// Class: SecurityCheckListItem
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:SecurityCheckListItem' => 'Правило',
+	'Class:SecurityCheckListItem+' => 'Элемент чеклиста',
+	'Class:SecurityCheckListItem/Attribute:securitychecklist_id' => 'Чеклист',
+	'Class:SecurityCheckListItem/Attribute:securitychecklist_id+' => '',
+	'Class:SecurityCheckListItem/Attribute:securitychecklist_name' => 'Чеклист',
+	'Class:SecurityCheckListItem/Attribute:securitychecklist_name+' => '',
+	'Class:SecurityCheckListItem/Attribute:scliequivalence_id' => 'Эквивалент',
+	'Class:SecurityCheckListItem/Attribute:scliequivalence_id+' => 'Элемент, задающий эквивалентность правил из разных чеклистов',
+	'Class:SecurityCheckListItem/Attribute:scliequivalence_name' => 'Эквивалент',
+	'Class:SecurityCheckListItem/Attribute:scliequivalence_name+' => 'Название элемента, задающего эквивалентность правил из разных чеклистов',
+	'Class:SecurityCheckListItem/Attribute:scliequivalence_description' => 'Описание эквивалента',
+	'Class:SecurityCheckListItem/Attribute:scliequivalence_description+' => 'Описание элемента, задающего эквивалентность правил из разных чеклистов',
+	'Class:SecurityCheckListItem/Attribute:scli_instance_list' => 'Проверки',
+	'Class:SecurityCheckListItem/Attribute:scli_instance_list+' => 'Список проверок, необходимых для данного правила',
+	'Class:SecurityCheckListItem/Tab:scli_instance_list' => 'Статус проверок',
+	'Class:SecurityCheckListItem/BlockHeader:Unassigned' => 'Не назначено',
+	'Class:SecurityCheckListItem/BlockHeader:Uncomplited' => 'Есть невыполненные проверки',
+	'Class:SecurityCheckListItem/BlockHeader:Complited' => 'Все проверки выполнены',
+));
+
+//
+// Class: OrganizationalSCLI
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:OrganizationalSCLI' => 'Орг. мера',
+	'Class:OrganizationalSCLI+' => 'Организационная мера - применимое к организации правило (элемент чеклиста).',
+	'Class:OrganizationalSCLI/Attribute:scope' => 'Область',
+	'Class:OrganizationalSCLI/Attribute:scope+' => 'Область применения (scope) правила - организация',
+));
+
+//
+// Class: FunctionalSCLI
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:FunctionalSCLI' => 'Тех. мера',
+	'Class:FunctionalSCLI+' => 'Техническая мера - применимое к функциональному КЕ правило (элемент чеклиста).',
+	'Class:FunctionalSCLI/Attribute:scope' => 'Область',
+	'Class:FunctionalSCLI/Attribute:scope+' => 'Область применения (scope) правила - группа КЕ.',
+));
+
+//
+// Class: SCLIEquivalence
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:SCLIEquivalence' => 'Эквивалент правил',
+	'Class:SCLIEquivalence+' => 'Элемент, задающий эквивалентность правил из разных чеклистов',
+	'Class:SCLIEquivalence/Attribute:scli_list' => 'Список правил',
+	'Class:SCLIEquivalence/Attribute:scli_list+' => 'Список эквивалентных правил',
+));
+
+//
+// Class: SecurityCheckListItemInstance
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:SecurityCheckListItemInstance' => 'Проверка',
+	'Class:SecurityCheckListItemInstance+' => 'Проверка (реализация) правила',
+	'Class:SecurityCheckListItemInstance/Attribute:securitychecklistitem_id' => 'Правило',
+	'Class:SecurityCheckListItemInstance/Attribute:securitychecklistitem_id+' => 'Элемент чеклиста',
+	'Class:SecurityCheckListItemInstance/Attribute:scli_name' => 'Правило',
+	'Class:SecurityCheckListItemInstance/Attribute:scli_name+' => '',
+	'Class:SecurityCheckListItemInstance/Attribute:scli_description' => 'Описание правила',
+	'Class:SecurityCheckListItemInstance/Attribute:scli_description+' => '',
+	'Class:SecurityCheckListItemInstance/Attribute:scliequivalence_name' => 'Эквивалент',
+	'Class:SecurityCheckListItemInstance/Attribute:scliequivalence_name+' => '',
+	'Class:SecurityCheckListItemInstance/Attribute:scliequivalence_description' => 'Описание эквивалента',
+	'Class:SecurityCheckListItemInstance/Attribute:scliequivalence_description+' => '',
+	'Class:SecurityCheckListItemInstance/Attribute:securitychecklist_id' => 'Чеклист',
+	'Class:SecurityCheckListItemInstance/Attribute:securitychecklist_id+' => '',
+	'Class:SecurityCheckListItemInstance/Attribute:securitychecklist_name' => 'Чеклист',
+	'Class:SecurityCheckListItemInstance/Attribute:securitychecklist_name+' => '',
+	'Class:SecurityCheckListItemInstance/Attribute:status' => 'Статус',
+	'Class:SecurityCheckListItemInstance/Attribute:status+' => '',
+	'Class:SecurityCheckListItemInstance/Attribute:status/Value:done' => 'Выполнено',
+	'Class:SecurityCheckListItemInstance/Attribute:status/Value:done+' => 'done',
+	'Class:SecurityCheckListItemInstance/Attribute:status/Value:not_done' => 'Не выполнено',
+	'Class:SecurityCheckListItemInstance/Attribute:status/Value:not_done+' => 'not done',
+	'Class:SecurityCheckListItemInstance/BlockHeader:Uncomplited' => 'Невыполненные проверки',
+	'Class:SecurityCheckListItemInstance/BlockHeader:Complited' => 'Выполненные проверки',
+));
+
+//
+// Class: InstanceOfOrganizationalSCLI
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:InstanceOfOrganizationalSCLI' => 'Проверка орг.меры',
+	'Class:InstanceOfOrganizationalSCLI+' => 'Проверка (реализация) организационного павила',
+	'Class:InstanceOfOrganizationalSCLI/BlockHeader:Uncomplited' => 'Невыполненные проверки',
+	'Class:InstanceOfOrganizationalSCLI/BlockHeader:Complited' => 'Выполненные проверки',
+));
+
+//
+// Class: InstanceOfFunctionalSCLI
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:InstanceOfFunctionalSCLI' => 'Проверка тех.меры',
+	'Class:InstanceOfFunctionalSCLI+' => 'Проверка (реализация) технического павила',
+	'Class:InstanceOfFunctionalSCLI/Attribute:functionalci_id' => 'КЕ',
+	'Class:InstanceOfFunctionalSCLI/Attribute:functionalci_id+' => 'КЕ к которому применяется проверка',
+	'Class:InstanceOfFunctionalSCLI/Attribute:functionalci_name' => 'КЕ',
+	'Class:InstanceOfFunctionalSCLI/Attribute:functionalci_name+' => 'КЕ к которому применяется проверка',
+	'Class:InstanceOfFunctionalSCLI/BlockHeader:Uncomplited' => 'Невыполненные проверки',
+	'Class:InstanceOfFunctionalSCLI/BlockHeader:Complited' => 'Выполненные проверки',
+));
+
+//
+// Class: SecurityCheckList
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:SecurityCheckList' => 'Чеклист',
+	'Class:SecurityCheckList+' => 'Список требований информационной безопасности',
+	'Class:SecurityCheckList/Attribute:scli_list' => 'Правила',
+	'Class:SecurityCheckList/Attribute:scli_list+' => 'Список правил проверки - элементы чеклиста',
+	'Class:SecurityCheckList/Tab:GroupedCheckListItems' => 'Статусы правил',
+));
+
+//
+// Class: lnkOrganizationToOrganizationalSCLI
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:lnkOrganizationToOrganizationalSCLI' => 'Связь:Организация/Орг.мера ИБ',
+	'Class:lnkOrganizationToOrganizationalSCLI+' => '',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:organizationalscli_id' => 'Орг.мера ИБ',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:organizationalscli_id+' => '',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:organizationalscli_name' => 'Орг.мера ИБ',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:organizationalscli_name+' => '',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:org_id' => 'Организация',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:org_id+' => '',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:org_name' => 'Организация',
+	'Class:lnkOrganizationToOrganizationalSCLI/Attribute:org_name+' => 'Название организации',
+));
+
+//
+// Class: lnkGroupToFunctionalSCLI
+//
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:lnkGroupToFunctionalSCLI' => 'Связь:Группа КЕ/Тех.мера ИБ',
+	'Class:lnkGroupToFunctionalSCLI+' => '',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:functionalscli_id' => 'Тех.мера ИБ',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:functionalscli_id+' => '',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:functionalscli_name' => 'Тех.мера ИБ',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:functionalscli_name+' => '',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:group_id' => 'Группа КЕ',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:group_id+' => '',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:group_name' => 'Группа КЕ',
+	'Class:lnkGroupToFunctionalSCLI/Attribute:group_name+' => '',
+));
 
 //
 // Application Menu
