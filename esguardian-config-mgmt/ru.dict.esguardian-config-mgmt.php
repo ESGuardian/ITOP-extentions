2c8761b85949add23e6ca97e347554fa09805859
Eugene Sokolov <esguardian@outlook.com>
2016-03-09 18:23:41 +0300

    Добавлен класс File Store

--- esguardian-config-mgmt/ru.dict.esguardian-config-mgmt.php
+++ esguardian-config-mgmt/ru.dict.esguardian-config-mgmt.php
@@ -5,12 +5,15 @@
  * @copyright   Copyright (C) 2013 XXXXX
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
+
 //
 // Class: ESGuardianFileStore
 //
 
-Dict::Add('EN US', 'English', 'English', array(
-	'Class:ESGuardianFileStore' => 'File Store',
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:ESGuardianFileStore' => 'Хранилище файлов',
 	'Class:ESGuardianFileStore+' => '',
+	
 ));
+
 ?>
